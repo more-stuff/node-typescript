7572a766906a599a3f15cf7baec2eb3c9b8a8293
Zehcnas <izan.perezsanchez@gmail.com>
2021-01-26 18:20:24 +0100

    arregalr bugs añadir gitignore y body parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-// lib/app.ts
-import express = require('express');
+import express from 'express'
+import bodyParser, { json } from 'body-parser'
 
 // Create a new express application instance
 const app: express.Application = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(json())
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
